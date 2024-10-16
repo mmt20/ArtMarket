0ce662aa4684532b5c3598c22fd0b0030a9f68ab
Abdelrahman_Abass <abdoabass400@gmail.com>
2024-10-16 16:02:31 +0300

    update

--- server_test/index.js
+++ server_test/index.js
@@ -14,7 +14,7 @@ import commonFeatureRouter from "./routes/common/feature-routes.js";
 
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+// const PORT = process.env.PORT || 5000;
 
 // Middleware to parse JSON requests
 app.use(cookieParser());
