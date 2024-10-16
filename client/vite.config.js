58e0570a6376be941bf6034a2d79c955116a2696
Abdelrahman_Abass <abdoabass400@gmail.com>
2024-10-16 18:56:18 +0300

    upload

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,6 +5,15 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://art-market-blue.vercel.app',
+        changeOrigin: true,
+        secure: false,
+      }
+    }
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
