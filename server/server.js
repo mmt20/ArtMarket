adabfb450990c1b2c0f8da15e32725d8e48127fb
Abdelrahman_Abass <abdoabass400@gmail.com>
2024-10-19 15:06:28 +0300

    update origins

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: 'https://art-market-fbss.vercel.app',
+    origin: ['https://art-market-fbss.vercel.app', "http://localhost:5173"],
     
     methods: ["GET", "POST", "DELETE", "PUT"],
     allowedHeaders: [
